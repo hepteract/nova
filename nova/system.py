f2a6120b5c5a9b5189fe202eb2749cef58cf1192
Ellie Eusebia <ellie@atercap.com>
2018-01-05 11:30:13 -0800

    Add system.py

---
+++ nova/system.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""System utility classes and functions"""
+
+import subprocess
+
+class System(object):
+    def __init__(self, method):
+        self.method = method
+
+    def __getattr__(self, name):
+        def system_wrapper(*args, **kwargs):
+            args = (name,) + args
+            return self.method(args, **kwargs)
+        return system_wrapper
+system = System(subprocess.call)
