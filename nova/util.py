395d5e0438bc1e7951dad9473939ad51986552fa
Ellie Eusebia <ellie@atercap.com>
2018-01-05 11:17:11 -0800

    Initialize util

---
+++ nova/util.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""Basic utilities for nova"""
+
+class _superpass(object):
+    def __getattr__(self, name):
+        return self
+    
+    def __setattr__(self, name, value):
+        return
+
+    def __delattr__(self, name):
+        return
+
+    def __getitem__(self, name):
+        return self
+
+    def __setitem__(self, name, value):
+        return
+
+    def __delitem__(self, name):
+        return
+
+    def __repr__(self):
+        return "superpass"
+
+    def __eq__(self, other):
+        return True
+
+    def __call__(self, *args, **kwargs):
+        return self
+superpass = _superpass()
