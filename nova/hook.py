962b3dad92ce07cb2f1c369523c34bc60d850717
Ellie Eusebia <ellie@atercap.com>
2018-02-15 09:08:43 -0800

    Add hook customization

--- nova/hook.py
+++ nova/hook.py
@@ -1,13 +1,25 @@
 """Simple API for registering hooks"""
 
 class HookTarget(object):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.hooks = ()
+
+        self.args = args
+        self.kwargs = kwargs
 
     def __call__(self, func):
         self.hooks += (func,)
         return func
 
     def run(self, *args, **kwargs):
-        for hook in self.hooks:
-            hook(*args, **kwargs)
+        args = self.args + args
+        if "check" in kwargs:
+            check = False
+            for hook in self.hooks:
+                if hook(*args, **self.kwargs) == kwargs["check"]:
+                    check = True
+            return check
+        else:
+            for hook in self.hooks:
+                hook(*args, **self.kwargs)
+            return
